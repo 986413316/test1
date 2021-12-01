d187c21a3952bcb048e76ff441deb70b46a88bf1
shilijun <986413316@qq.com>
2021-12-01 10:37:19 +0800

    第二提交次

---
+++ untitled/src/ds.java
@@ -0,0 +1,12 @@
+/**
+ * @author shilijun
+ * @create 2021-12-01-10:29
+ */
+public class ds {
+    public static void main(String[] args) {
+        System.out.println("ss");
+        System.out.println("yes");
+        System.out.println("yes");
+    }
+}
+

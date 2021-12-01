c0346c3b94912734f0c9fd4b6ff64612c4b14744
shilijun <986413316@qq.com>
2021-12-01 10:25:47 +0800

    dss

---
+++ untitled/src/student.java
@@ -0,0 +1,11 @@
+/**
+ * @author shilijun
+ * @create 2021-12-01-9:47
+ */
+public class student {
+    public static void main(String[] args) {
+        System.out.println("a student");
+        System.out.println(" on");
+    }
+
+}
